157c0f4066aa77277d7f6940c074e2257e5bff7e
Harlan T Wood <harlan@thegoldensun.com>
2015-02-14 10:44:04 -0800

    Compile coffee to JS before NPM publish; use the compiled JS only in the installed version.  Ignore JS locally; ignore coffee when installed.

--- src/exports.js
+++
@@ -1,8 +0,0 @@
-try {
-  // this fails in webpack, but is not needed there
-  // it is needed by Node.js
-  require('coffee-script/register');
-}
-catch(e) {}
-
-module.exports = require('./core/nodesphere.coffee');
