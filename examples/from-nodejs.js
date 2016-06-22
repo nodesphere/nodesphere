920b31b4e517738149fb515f0411476fa7dd5f0a
Harlan T Wood <code+harlan@harlantwood.net>
2016-06-21 21:35:35 -0600

    remove commented out example code

--- examples/from-nodejs.js
+++ examples/from-nodejs.js
@@ -4,7 +4,6 @@ d = require('lightsaber/lib/log').d
 
 Nodesphere = require('..')
 Metamaps = Nodesphere.adaptor.Metamaps
-Ipfs = Nodesphere.adaptor.Ipfs
 
 protocol = 'https://'
 domain = 'metamaps.herokuapp.com'
@@ -23,27 +22,3 @@ return Ipfs.create()
 }).catch(function(error) {
   console.error(error.stack);
 })
-
-
-// metamaps.fetch({protocol, domain, mapId})
-// .then (function(source) {
-//   this.source = source
-//   console.log('Retrieved data for:')
-//   console.log(source.id)
-//   // return Ipfs.create({protocol: 'http', host: 'v03x.ipfs.io', port: 80})
-//   return Ipfs.create()
-// }).then(function(target) {
-//   return target.put({
-//     content: this.source.toJson()
-//   })
-// }).then(function(ipfsHashes) {
-//   // console.log(ipfsHashes)
-//   console.log("https://ipfs.io/ipfs/" + ipfsHashes[0])
-// // }).error(function(error) {
-// //   throw error
-// //   // console.error(error.stack);
-// }).catch(function(error) {
-//   console.error(error.stack);
-//   // throw error
-//   // console.error(error.stack);
-// })
